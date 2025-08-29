733c4f2216ad186d6f12939a353862635ea197ff
Aslı Nur Aksakal <92970740+asliak@users.noreply.github.com>
2025-08-29 21:09:44 +0300

    Add files via upload

---
+++ 8-Database Programming.py
@@ -0,0 +1,82 @@
+import sqlite3
+class Person:
+    def __init__(self, id_number = -1, first = "", last = "", age = -1):
+        self.id_number = id_number
+        self.first = first
+        self.last = last
+        self.age = age
+        self.connection = sqlite3.connect('mydata1.db')
+        self.cursor = self.connection.cursor()
+
+
+    def load_person(self, id_number):
+        self.cursor.execute("""
+        SELECT * FROM persons
+        WHERE id = {}
+                       
+                       """.format(id_number))
+        results = self.cursor.fetchone()
+
+        self.id_number = id_number
+        self.first = results[1]
+        self.last = results[2]
+        self.age = results[3]
+    
+    def insert_person(self):
+        self.cursor.execute("""
+        INSERT INTO persons VALUES
+        ({}, '{}', '{}', {})
+
+        """.format(self.id_number, self.first, self.last, self.age))
+
+        self.connection.commit()
+        self.connection.close()
+
+# passing values from database to python environment
+#p1 = Person()
+#p1.load_person(1)
+#print(p1.first)
+#print(p1.last)
+#print(p1.age)
+#print(p1.id_number)
+
+# passing values as python codes to th database's rows
+p1 = Person(7, 'Alex', 'Robbins', 30)
+p1.insert_person()
+
+connection = sqlite3.connect('mydata1.db')
+cursor = connection.cursor()
+
+cursor.execute("SELECT * FROM persons")
+results = cursor.fetchall()
+print(results)
+connection.close()
+
+
+
+#
+#cursor.execute("""
+#CREATE TABLE IF NOT EXISTS persons (
+#               id INTEGER PRIMARY KEY AUTOINCREMENT,
+#               first_name TEXT,
+#               last_name TEXT,
+#               age INTEGER
+#               )
+#               
+#               """)
+#cursor.execute("""
+#INSERT INTO persons VALUES
+#            (1, 'Paul','Smith',24),
+#            (2, 'Mark','Johnson',42),
+#            (3, 'Anna','Smith',34)
+#               """)
+#
+#cursor.execute("""
+#SELECT * FROM persons
+#WHERE last_name = 'Smith'
+#               """)
+#rows = cursor.fetchall()
+#print(rows)
+#connection.commit()
+#connection.close()
+
