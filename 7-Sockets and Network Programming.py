68cb61dfa70c9c747609f37d3f618642990fdeb2
Aslı Nur Aksakal <92970740+asliak@users.noreply.github.com>
2025-08-29 20:29:41 +0300

    Add files via upload

---
+++ 7-Sockets and Network Programming.py
@@ -0,0 +1,12 @@
+import socket
+# an internet socket working with tcp
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind(('127.0.0.1', 55555))
+s.listen()
+
+while True:
+    client, address = s.accept()
+    print("Connected to {}".format(address))
+    client.send("You are connected!".encode())
+    client.close()
+
